0117a9b38e23149c723a1475c4a4e57d553f3b25
Janni235 <92309480+Janni235@users.noreply.github.com>
2022-12-15 09:28:34 +0000

    Add files via upload
    initial commit

---
+++ src/tabledata.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Extension\Janni235\BoltTableData;
+
+use Bolt\Field\FieldInterface;
+use Bolt\Field\FieldTypeBase;
+use Bolt\Field\FieldOption as Option;
+use Bolt\Translation\Translator as Trans;
+
+class TableDataType extends FieldTypeBase implements FieldInterface
+{
+    public function getName()
+    {
+        return 'tabledata';
+    }
+
+    public function getTemplate()
+    {
+        return 'tabledata.twig';
+    }
+
+    public function getStorageType()
+    {
+        return 'text';
+    }
+
+    public function getStorageOptions()
+    {
+        return [
+            'default' => null,
+            'notnull' => false
+        ];
+    }
+
+    public function getStorageColumnType()
+    {
+        return 'text';
+    }
+
+    public function getFormType()
+    {
+        return 'tabledata';
+    }
+
+    public function getFormOptions()
+    {
+        return [
+            'label' => 'Table data',
+            'default' => null,
+            'required' => false,
+            'choices' => [
+                'row' => 'Row',
+                'column' => 'Column'
+            ],
+        ];
+    }
+
+    public function getValidationOptions()
+    {
+        return [
+            'required' => false,
+            'minlength' => null,
+            'maxlength' => null,
+            'pattern' => null,
+        ];
+    }
+
+    public function getDefaultValue()
+    {
+        return null;
+    }
+
+    public function getTemplateOptions()
+    {
+        return [
+            'tabledata' => [
+                'form' => 'tabledata.twig',
+                'display' => 'tabledata.twig',
+            ]
+        ];
+    }
+
+    public function getConfiguration()
+    {
+        return [
+            'type' => $this->getName(),
+            'form' => [
+                'type' => $this->getFormType(),
+                'options' => $this->getFormOptions()
+            ],
+            'template' => [
+                'name' => $this->getTemplate(),
+                'options' => $this->getTemplateOptions()
+            ],
+            'storage' => [
+                'type' => $this->getStorageType(),
+                'options' => $this->getStorageOptions()
+            ],
+            'validation' => [
+                'options' => $this->getValidationOptions()
+            ]
+        ];
+    }
+}
+
